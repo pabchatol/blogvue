276446e65576a58933ef0b85fd87086299f8d680
pabchatol <122971278+pabchatol@users.noreply.github.com>
2023-05-10 09:49:59 +0000

    Deploying to gh-pages from  @ e2d9e156b2b3a0af9a2a63ca07f0d9e66071fcf1 🚀

---
+++ assets/AboutView-3cc5eaf4.js
@@ -0,0 +1,1 @@
+import{o as a,c as t,a as s,F as o,b as e,N as _}from"./index-179cf51e.js";const n=e("div",{class:"about"},[e("h1",null,"This is an about page")],-1),i={__name:"AboutView",setup(c){return(r,u)=>(a(),t(o,null,[s(_),n],64))}};export{i as default};
